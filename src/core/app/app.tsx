65092f0d387c713d064d7b320778ffb945fa2806
roman_developer <89508006+RomanDeveloperGit@users.noreply.github.com>
2023-10-15 16:40:31 +0300

    fix(*): fix styles

--- src/core/app/app.tsx
+++ src/core/app/app.tsx
@@ -4,12 +4,14 @@ import { Notifications } from '@/modules/notifications';
 // eslint-disable-next-line import/no-unassigned-import
 import 'normalize.css';
 
+import styles from './styles.module.scss';
+
 export const App = () => {
   // sentry ( повешать листенер на все реджекты )
   // api ???
 
   return (
-    <div>
+    <div className={styles.container}>
       <Forms />
       <Notifications />
     </div>
